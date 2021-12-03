a99dccf1c0dbc62cf0115e948fdc0ffa3190f148
SungGeun Park <sg2pooh@gmail.com>
2021-12-03 15:07:18 +0900

    temp-cart 수정 중

--- htashop/src/main/java/com/shop/dao/TempCartDao.java
+++ htashop/src/main/java/com/shop/dao/TempCartDao.java
@@ -20,25 +20,49 @@ public class TempCartDao {
 		return self;
 	}
 	
-	public void insertCart(Cart cart) throws SQLException {
-		String sql = "insert into shop_cart(cart_no, user_no, product_no, cart_quantity) "
-				   + "values (shop_cart_seq.nextval, ?, ?, ?) ";
+	/**
+	 * 사용자 번호로 사용자가 담은 카트를 조회한다.
+	 * @param userNo 사용자 번호
+	 * @return 사용자가 담은 카트 리스트
+	 * @throws SQLException
+	 */
+	public List<Cart> getAllCartListByUserNo(int userNo) throws SQLException {
+		String sql = "select * "
+				   + "from shop_cart "
+				   + "where user_no = ? ";
+		
+		List<Cart> carts = new ArrayList<Cart>();
 		
 		Connection connection = getConnection();
 		PreparedStatement pstmt = connection.prepareStatement(sql);
-		pstmt.setInt(1, cart.getUserNo());
-		pstmt.setInt(2, cart.getProductNo());
-		pstmt.setInt(3, cart.getQuantity());
+		pstmt.setInt(1, userNo);
+		ResultSet rs = pstmt.executeQuery();
 		
-		pstmt.executeUpdate();
+		while (rs.next()) {
+			Cart cart = new Cart();
+			cart.setUserNo(rs.getInt("user_no"));
+			cart.setCartNo(rs.getInt("cart_no"));
+			cart.setProductNo(rs.getInt("product_no"));
+			cart.setQuantity(rs.getInt("cart_quantity"));
+			
+			carts.add(cart);
+		}
 		
+		rs.close();
 		pstmt.close();
 		connection.close();
+
+		return carts;
 	}
 	
+	/**
+	 * 모든 사용자가 담은 카트를 조회한다.
+	 * @return 모든 사용자가 담은 카트 리스트
+	 * @throws SQLException
+	 */
 	public List<Cart> getAllCartList() throws SQLException {
 		String sql = "select * "
-				   + "from shop_cart ";
+				+ "from shop_cart ";
 		
 		List<Cart> carts = new ArrayList<Cart>();
 		
@@ -59,9 +83,44 @@ public class TempCartDao {
 		rs.close();
 		pstmt.close();
 		connection.close();
-
+		
 		return carts;
 	}
+	
+	public void insertCart(Cart cart) throws SQLException {
+		String sql = "insert into shop_cart(cart_no, user_no, product_no, cart_quantity) "
+				+ "values (shop_cart_seq.nextval, ?, ?, ?) ";
+		
+		Connection connection = getConnection();
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+		pstmt.setInt(1, cart.getUserNo());
+		pstmt.setInt(2, cart.getProductNo());
+		pstmt.setInt(3, cart.getQuantity());
+		
+		pstmt.executeUpdate();
+		
+		pstmt.close();
+		connection.close();
+	}
+	
+	public void updateCart(Cart cart) throws SQLException {
+		String sql = "update shop_cart "
+				   + "set "
+				   + "	cart_quantity = ? "
+				   + "where cart_no = ? ";
+		
+		Connection connection = getConnection();
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+		pstmt.setInt(1, cart.getQuantity());
+		pstmt.setInt(2, cart.getCartNo());
+		
+		pstmt.executeUpdate();
+		
+		pstmt.close();
+		connection.close();
+	}
+	
+	
 }
 
 
