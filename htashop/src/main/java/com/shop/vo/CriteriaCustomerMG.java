b88b54a74950d8ccd5cd5748c8c38f28ac5e3fc2
SungGeun Park <sg2pooh@gmail.com>
2021-12-07 15:15:58 +0900

    관리자 고객관리 최종_ver.1

--- htashop/src/main/java/com/shop/vo/CriteriaCustomerMG.java
+++ htashop/src/main/java/com/shop/vo/CriteriaCustomerMG.java
@@ -1,41 +1,78 @@
 package com.shop.vo;
 
 public class CriteriaCustomerMG {
-	private String option;
-	private String keyword;
+	private String privateInfo;
+	private String privateInfoKeyword;
+	private String grade;
+	private String minDate;
+	private String maxDate;
+	private String minAge;
+	private String maxAge;
+	private String gender;
 	private int beginIndex;
 	private int endIndex;
-
-	public String getOption() {
-		return option;
+	
+	public String getPrivateInfo() {
+		return privateInfo;
 	}
-
-	public void setOption(String option) {
-		this.option = option;
+	public void setPrivateInfo(String privateInfo) {
+		this.privateInfo = privateInfo;
 	}
-
-	public String getKeyword() {
-		return keyword;
+	public String getPrivateInfoKeyword() {
+		return privateInfoKeyword;
 	}
-
-	public void setKeyword(String keyword) {
-		this.keyword = keyword;
+	public void setPrivateInfoKeyword(String privateInfoKeyword) {
+		this.privateInfoKeyword = privateInfoKeyword;
 	}
-
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getMinDate() {
+		return minDate;
+	}
+	public void setMinDate(String minDate) {
+		this.minDate = minDate;
+	}
+	public String getMaxDate() {
+		return maxDate;
+	}
+	public void setMaxDate(String maxDate) {
+		this.maxDate = maxDate;
+	}
+	public String getMinAge() {
+		return minAge;
+	}
+	public void setMinAge(String minAge) {
+		this.minAge = minAge;
+	}
+	public String getMaxAge() {
+		return maxAge;
+	}
+	public void setMaxAge(String maxAge) {
+		this.maxAge = maxAge;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 	public int getBeginIndex() {
 		return beginIndex;
 	}
-
 	public void setBeginIndex(int beginIndex) {
 		this.beginIndex = beginIndex;
 	}
-
 	public int getEndIndex() {
 		return endIndex;
 	}
-
 	public void setEndIndex(int endIndex) {
 		this.endIndex = endIndex;
 	}
-
+	
+	
+	
 }
--- htashop/src/main/webapp/common/admin-left.jsp
+++ htashop/src/main/webapp/common/admin-left.jsp
@@ -1,15 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%
-String leftMenu = (String)pageContext.getAttribute("leftMenu");
+	String leftMenu = (String)pageContext.getAttribute("leftMenu");
 %>
-<!-- 상단 네비 메뉴 마다 레프트 메뉴가 달라진다. 레프트 메뉴 기본형식을 유지하며 조건에 따라 레프트 메뉴를 표시해야 한다. 
-<ul class="list-group mt-3">
-	<li class="list-group-item"><a href="#" class="nav-link align-middle">Home</a></li>
-	<li class="list-group-item"><a href="#" class="nav-link align-middle"><span class="ms-1 d-none d-sm-inline">Home</span></a></li>
-	<li class="list-group-item"><a href="#" class="nav-link align-middle"><span class="ms-1 d-none d-sm-inline">Home</span></a></li>
-</ul>
- -->
- 
 <ul class="list-group mt-3">
 <%
 	if ("home".equals(leftMenu)) {
